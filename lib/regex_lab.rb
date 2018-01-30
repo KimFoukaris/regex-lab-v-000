d332518f0cf6c2bc4c0e8484efce0c8c12c6de6f
Kim Foukaris <kimfoukaris@outlook.com>
2018-01-30 13:53:42 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,9 +17,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  string = text.match(/\A[A-Z][a-zA-Z\s\S]*[;!.?]$/)
-  puts "string"
-  if string == nil
+  if string = text.match(/\A[A-Z][a-zA-Z\s\S]*[;!.?]$/) == nil
     false
   else
     true
