d08d878d291505a7e393dde8618e24a755427dfe
Kim Foukaris <kimfoukaris@outlook.com>
2018-01-30 12:47:44 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,7 +12,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
- list = text.scan(/\b[a-zA-Z]{3}\b/)
+ list = text.scan(/\b[a-zA-Z]{5}\b/)
  list
 end
 
