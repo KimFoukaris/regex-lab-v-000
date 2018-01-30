c6d765ad44270d984af0af234dffd8ef7d68f402
Kim Foukaris <kimfoukaris@outlook.com>
2018-01-30 12:42:24 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
- list = word.scan(/\bun[a-zA-Z]*ing\b/)
+ list = text.scan(/\bun[a-zA-Z]*ing\b/)
  list
 end
 
